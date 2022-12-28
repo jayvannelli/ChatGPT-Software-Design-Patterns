7186fdd359604489a425076fc7c4be0a48a15cd1
jayvannelli <jaymvannelli@gmail.com>
2022-12-28 10:30:30 -0500

    Fix name.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# OpenGPT | Software Design Patterns
+# ChatGPT | Software Design Patterns
 
-This streamlit application was made to show what OpenGPT responds with
+This streamlit application was made to show what ChatGPT responds with
 when asked to describe and show an example of different software design patterns.
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 
 def main():
-    st.title("OpenGPT | Software Design Patterns")
+    st.title("ChatGPT | Software Design Patterns")
     st.write("""
-        This streamlit application showcases OpenGPT and its ability to 
+        This streamlit application showcases ChatGPT and its ability to 
         explain and show examples/implementations of common software design
         patterns.
     """)
